1da617fb616b8e9d7485c9390d939f029b11b946
Allah Nawaz <nawaz51412@gmail.com>
2026-02-11 17:53:08 +0500

    login updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -44,7 +44,7 @@ const Login = () => {
               </label>
               <input
                 type="text"
-                placeholder="Enter username"
+                placeholder="admin"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 className="w-full border border-gray-300 rounded-lg px-3 py-2.5 text-sm md:text-base focus:outline-none focus:ring-2 focus:ring-blue-300"
@@ -58,7 +58,7 @@ const Login = () => {
               </label>
               <input
                 type="password"
-                placeholder="Enter password"
+                placeholder="useradmin"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 className="w-full border border-gray-300 rounded-lg px-3 py-2.5 text-sm md:text-base focus:outline-none focus:ring-2 focus:ring-blue-300"
